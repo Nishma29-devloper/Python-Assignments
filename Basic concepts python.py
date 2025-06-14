1e9cdcca76cc87d7daf36ad7a1e45629c1184506
Nishma29-devloper <ece.engineer015@gmail.com>
2025-06-14 12:57:08 +0530

    Add files via upload

---
+++ Basic concepts python.py
@@ -0,0 +1,19 @@
+#Task-1 Perform with the basic mathematical operations
+
+a = int(input("Enter a First Number:"))
+b = int(input("Enter a Second Number:"))
+
+#performing operations
+
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
+
+
+
+#Task -2 Create a Personalized Greeting
+
+fname = input("Enter your First Name:")
+lname = input("Enter your Last Name: ")
+print(f"Hello, {fname} {lname}! Welcome to the python program")
